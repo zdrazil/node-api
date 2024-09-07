64caa74ed5f2344dc594d9811e219d84d4d428fb
Vladimír Zdražil <git@vzdrazil.com>
2024-09-07 20:48:20 +0200

    Remove unused packages and make package versions exact

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,3 @@
-const sqlSettings = {
-  dataTypeCase: 'upper',
-  embeddedSqlTags: ['sql'],
-  expressionWidth: 150,
-  functionCase: 'lower',
-  keywordCase: 'upper',
-  language: 'postgresql',
-};
-
 /**
  * @type {import('prettier').Config}
  * @see https://prettier.io/docs/en/configuration.html
@@ -18,11 +9,9 @@ const config = {
   plugins: [
     '@ianvs/prettier-plugin-sort-imports',
     'prettier-plugin-jsdoc',
-    // 'prettier-plugin-sql',
     'prettier-plugin-embed',
     'prettier-plugin-packagejson',
   ],
-  // ...sqlSettings,
   proseWrap: 'never',
   singleQuote: true,
   trailingComma: 'all',
